e8b8664d9dfcc486fa4ddd36deb2f184839efa27
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2024-03-06 20:52:01 +0100

    Initial commit

---
+++ components/Container.js
@@ -0,0 +1,197 @@
+import Image from "next/image";
+import Link from "next/link";
+import { motion } from "framer-motion";
+import clsx from "clsx";
+import { turnObjectIntoString } from "helpers/manipulateText";
+
+export const Container = ({ children, as, className, ...props }) => {
+  let Component = as ?? "div";
+  const classNameProp = turnObjectIntoString(className);
+
+  return (
+    <Component className={clsx(classNameProp)} {...props}>
+      {children}
+    </Component>
+  );
+};
+
+export function AnimatedContainer({ children, className, ...props }) {
+  const classNameProps = turnObjectIntoString(className);
+
+  return (
+    <motion.div {...props} className={clsx(classNameProps)}>
+      {children}
+    </motion.div>
+  );
+}
+
+Container.Section = function ContainerSection({
+  children,
+  className,
+  as,
+  bottomDiv,
+  ...props
+}) {
+  let Component = as ?? "section";
+  const classNameProp = turnObjectIntoString(className);
+
+  return (
+    <Component
+      className={clsx(
+        classNameProp,
+        bottomDiv ? "pb-14" : "pb-24 sm:pb-32",
+        "relative isolate mx-auto max-w-4xl desktop-sm:max-w-5xl px-6 pt-10 lg:px-8"
+      )}
+      {...props}
+    >
+      {children}
+    </Component>
+  );
+};
+
+Container.Columns = function ContainerColumns({
+  children,
+  className,
+  ...props
+}) {
+  const classNameProp = turnObjectIntoString(className);
+  return (
+    <div className={clsx(classNameProp, "grid")} {...props}>
+      {children}
+    </div>
+  );
+};
+
+Container.Flex = function ContainerFlex({ children, className, ...props }) {
+  const classNameProp = turnObjectIntoString(className);
+  return (
+    <div
+      className={clsx(
+        classNameProp,
+        "flex",
+        className?.flex === undefined && "flex-row justify-between items-center"
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};
+
+Container.Logo = function ContainerLogo({ className, ...props }) {
+  const classNameProp = turnObjectIntoString(className);
+
+  return (
+    <Image
+      className={clsx(classNameProp, "rounded-full")}
+      src={props.src}
+      alt={props.alt}
+    />
+  );
+};
+
+Container.Link = function ContainerLink({
+  children,
+  text,
+  className,
+  href,
+  onClick,
+  Component,
+  componentProps,
+  ...props
+}) {
+  const classNameParent = turnObjectIntoString(className?.parent);
+  const classNameChild = turnObjectIntoString(className?.child);
+
+  return (
+    <Link
+      href={href}
+      className={clsx(classNameParent, "cursor-pointer")}
+      onClick={onClick}
+      {...props}
+    >
+      {Component && (
+        <Component
+          className={clsx(classNameChild, "w-auto h-6 fill-zinc-500")}
+          {...componentProps}
+        />
+      )}
+      {text || children}
+    </Link>
+  );
+};
+
+Container.List = function ContainerList({
+  as,
+  list,
+  className,
+  AdditionalComponent,
+}) {
+  const ParentComponent = as.parent ?? "ul";
+  const classNamePropParent = turnObjectIntoString(className?.parent);
+
+  const ChildComponent = as.child ?? "li";
+  const classNamePropChild = turnObjectIntoString(className?.child);
+
+  return (
+    <ParentComponent
+      role="list"
+      className={clsx(classNamePropParent, "leading-6")}
+    >
+      {list.map((item) => (
+        <ChildComponent key={item.id} className={clsx(classNamePropChild)}>
+          {AdditionalComponent ? <AdditionalComponent item={item} /> : item}
+        </ChildComponent>
+      ))}
+    </ParentComponent>
+  );
+};
+
+Container.Table = function ContainerTable({ table, className }) {
+  return (
+    <table
+      className={clsx(
+        className.table,
+        "max-w-sm mx-auto whitespace-nowrap mb-5"
+      )}
+    >
+      <colgroup>
+        {table.th.map((header, index) => (
+          <col
+            key={header}
+            className={clsx(
+              className.col,
+              index === 0 ? className.col.first : className.col.rest
+            )}
+          />
+        ))}
+      </colgroup>
+      <thead className={clsx(className.thead, "text-sm")}>
+        <tr>
+          {table.th.map((header) => (
+            <th key={header} scope="col" className={clsx(className.th)}>
+              {header}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody className={className.tbody}>
+        {table.tr.map((row, index) => (
+          <tr key={row}>
+            {row.td.map((cell) => (
+              <td key={index}>{cell}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+Container.Image = function ContainerImage({ src, alt, className, ...props }) {
+  const classNameProp = turnObjectIntoString(className);
+
+  return (
+    <Image className={clsx(classNameProp)} src={src} alt={alt} {...props} />
+  );
+};
---
+++ components/TextLayout.js
@@ -0,0 +1,109 @@
+import clsx from "clsx";
+import { turnObjectIntoString } from "helpers/manipulateText";
+import { motion } from "framer-motion";
+
+export const TextLayout = ({ className, children }) => {
+  const classNameProp = turnObjectIntoString(className);
+  return (
+    <div className={clsx(classNameProp, "text-zinc-500 dark:text-gray-200")}>
+      {children}
+    </div>
+  );
+};
+
+TextLayout.Title = function TextLayoutTitle({
+  as,
+  title,
+  AdditionalComponent,
+  className,
+  props,
+}) {
+  const Component = as ?? "h1";
+  const classNameProp = turnObjectIntoString(className);
+
+  const variants = {
+    h1: "text-4xl font-bold sm:text-5xl text-zinc-900 dark:text-zinc-100",
+    h2: "text-2xl sm:text-3xl text-zinc-900 dark:text-zinc-100",
+    h3: "text-xl sm:text-2xl text-zinc-900 dark:text-zinc-100",
+    h4: "text-sm font-semibold text-zinc-900 dark:text-zinc-100",
+  };
+
+  return (
+    <Component className={clsx(classNameProp, variants[as])} {...props}>
+      {AdditionalComponent}
+      {title}
+    </Component>
+  );
+};
+
+TextLayout.Subtitle = function TextLayoutSubtitle({
+  as,
+  subtitle,
+  className,
+  ...props
+}) {
+  const Component = as ?? "h2";
+  const classNameProp = turnObjectIntoString(className);
+
+  return (
+    <Component
+      className={clsx(classNameProp, "text-2xl leading-8 ")}
+      {...props}
+    >
+      {subtitle}
+    </Component>
+  );
+};
+
+TextLayout.Paragraph = function TextLayoutParagraph({
+  paragraph,
+  children,
+  className,
+  ...props
+}) {
+  const classNameProp = turnObjectIntoString(className);
+
+  return (
+    <p
+      className={clsx(classNameProp, "text-zinc-600 dark:text-zinc-400")}
+      {...props}
+    >
+      {paragraph || children}
+    </p>
+  );
+};
+
+TextLayout.Tag = function TextLayoutTag({ tag, className }) {
+  const classNameProp = turnObjectIntoString(className);
+  return (
+    <div className="bg-zinc-700 dark:bg-gray-200 rounded-full h-fit w-fit px-3">
+      <p
+        className={clsx(
+          classNameProp,
+          " text-gray-300 dark:text-gray-400 text-xs"
+        )}
+      >
+        #{tag}
+      </p>
+    </div>
+  );
+};
+
+TextLayout.Number = function TextLayoutParagraph({
+  number,
+  children,
+  className,
+  ...props
+}) {
+  const classNameProp = turnObjectIntoString(className);
+
+  return (
+    <motion.p
+      {...props.animations}
+      className={clsx(classNameProp, "text-zinc-600 dark:text-zinc-400")}
+      {...props}
+    >
+      {number || children}
+    </motion.p>
+  );
+};
