82471c6a9cdc92aac05482f4914cf6656176c1ab
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2024-03-10 19:18:50 +0100

    Add Hero component

---
+++ components/Hero.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import TextLayout from "./TextLayout";
+import { Container } from "./Container";
+
+const Hero = () => {
+  return (
+    <Container className={" bg-blue-secondary px-10 py-10"}>
+      <TextLayout.Title as="h1" title="Your Appointments!" />
+      <TextLayout.Paragraph
+        paragraph="Welcome to your appointments dashboard. Here you can see your upcoming appointments and schedule new ones."
+        className="mt-5"
+      />
+    </Container>
+  );
+};
+
+export default Hero;
