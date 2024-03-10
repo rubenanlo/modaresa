f49f7c1a281f90aa4947b2b770a3ba14f4309628
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2024-03-10 22:50:41 +0100

    Refactor UpNext component to use NextAppointment interface

--- components/UpNext.tsx
+++ components/UpNext.tsx
@@ -1,14 +1,18 @@
 import { Container } from "./Container";
 import TextLayout from "./TextLayout";
 
+interface NextAppointment {
+  title: string;
+  type: string;
+  location?: string;
+  startTime: string;
+  endTime: string;
+  vendorName: string;
+  buyerName: string;
+}
+
 interface UpNextProps {
-  nextAppointment: {
-    title: string;
-    type: string;
-    location?: string;
-    startTime: string;
-    endTime: string;
-  };
+  nextAppointment: NextAppointment;
 }
 
 const UpNext: React.FC<UpNextProps> = ({ nextAppointment }: UpNextProps) => {
@@ -17,27 +21,27 @@ const UpNext: React.FC<UpNextProps> = ({ nextAppointment }: UpNextProps) => {
       className={{ dimension: "h-52", flex: "flex-col justify-center gap-y-2" }}
     >
       <Container>
-        <TextLayout.Title as="h3" title={nextAppointment?.title} />
-        <TextLayout.Paragraph paragraph={nextAppointment?.time} />
+        <TextLayout.Title as="h3" title={nextAppointment.title} />
+        <TextLayout.Paragraph paragraph={nextAppointment.startTime} />
       </Container>
       <Container className="w-10 border-b" />
       <Container className="italic">
         <TextLayout.Paragraph
-          paragraph={`Type of appointment: ${nextAppointment?.type}`}
+          paragraph={`Type of appointment: ${nextAppointment.type}`}
         />
-        {nextAppointment?.type === "PHYSICAL" && (
+        {nextAppointment.type === "PHYSICAL" && (
           <TextLayout.Paragraph
-            paragraph={`Location: ${nextAppointment?.location}`}
+            paragraph={`Location: ${nextAppointment.location}`}
           />
         )}
       </Container>
       <Container className="w-10 border-b" />
       <Container className="font-bold">
         <TextLayout.Paragraph
-          paragraph={`Vendor: ${nextAppointment?.vendorName}`}
+          paragraph={`Vendor: ${nextAppointment.vendorName}`}
         />
         <TextLayout.Paragraph
-          paragraph={`Buyer: ${nextAppointment?.buyerName}`}
+          paragraph={`Buyer: ${nextAppointment.buyerName}`}
         />
       </Container>
     </Container.Flex>
