5bda1ba8e60764c7b63ff03411a1ff25488709dd
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2024-03-12 10:33:13 +0100

    Add TypeScript ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,24 +9,36 @@ module.exports = {
     "plugin:react/recommended",
     "next/core-web-vitals",
     "prettier",
+    "plugin:@typescript-eslint/recommended", // Add TypeScript recommended rules
   ],
+  parser: "@typescript-eslint/parser", // Specify TypeScript parser
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
-      tsx: true,
     },
     ecmaVersion: 12,
     sourceType: "module",
+    project: "./tsconfig.json", // Specify the path to your tsconfig.json file
   },
   settings: {
     react: {
       version: "detect",
     },
     "import/resolver": {
-      // Tells ESLint to use the TypeScript resolver
-      typescript: {}, // No need for any specific configuration options
+      typescript: {}, // Tell ESLint to use the TypeScript resolver
     },
   },
-  plugins: ["react", "no-only-tests"],
-  rules: { "no-only-tests/no-only-tests": "error" },
+  plugins: ["react", "no-only-tests", "@typescript-eslint"], // Add TypeScript plugin
+  rules: {
+    "no-only-tests/no-only-tests": "error",
+    "@typescript-eslint/explicit-module-boundary-types": "off", // Disable explicit module boundary types for functions
+  },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"], // Apply TypeScript rules to TypeScript files
+      rules: {
+        "@typescript-eslint/explicit-module-boundary-types": "off", // Adjust as needed
+      },
+    },
+  ],
 };
