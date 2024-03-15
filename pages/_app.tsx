c7e4b250cdf5683d18d5b044bed423fe27a33867
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2024-03-15 17:35:26 +0100

    Remove Google Analytics tracking code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,10 @@
-import { useEffect } from "react";
 import Head from "next/head";
 import { DefaultSeo } from "next-seo";
 import { TITLE, META_DESCRIPTION, META_IMAGE, URL } from "../config";
-import { Router } from "next/router";
-import * as gtag from "helpers/gtag";
-import RootProviders from "providers/rootProviders";
+import RootProviders from "../providers/rootProviders";
 import "styles/globals.css";
 
 const App = ({ Component, pageProps }) => {
-  // Track pages with google analytics
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url);
-    };
-    Router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      Router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, []);
-  // End of track pages with google analytics
-
   return (
     <>
       <Head>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,35 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
-import { GA_TRACKING_ID } from "root/config";
-import { ICON } from "root/config";
+import { ICON } from "../config";
 
 const Document = () => (
   <Html lang="en">
     <Head>
       <link rel="icon" href={ICON} />
-
-      {/* Global Site Tag - Google Analytics */}
-      <Script
-        async
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-      />
-
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${GA_TRACKING_ID}', {
-                  page_path: window.location.pathname,
-                });
-              `,
-        }}
-      />
-      {/* End of Global Site Tag - Google Analytics */}
     </Head>
     <body>
       <Main />
